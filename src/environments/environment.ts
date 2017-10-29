9ce8de93610c1d7a180c7eefe6d3731f9e7e196b
Michael Mahrt <mikem255@gmail.com>
2017-10-29 07:26:15 -0700

    finished app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  PIXABAY_API_KEY: '6863208-b5029e94851bcced80fa18c1d',
+  PIXABAY_API_URL: 'https://pixabay.com/api/?key='
 };

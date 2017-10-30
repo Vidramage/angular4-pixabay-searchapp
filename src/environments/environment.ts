dd5c398a826b0d3627515e12b5c977c617ba46c9
Michael Mahrt <mikem255@gmail.com>
2017-10-29 19:05:14 -0700

    removed api key - use your own

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: false,
-  PIXABAY_API_KEY: '6863208-b5029e94851bcced80fa18c1d',
+  PIXABAY_API_KEY: '',
   PIXABAY_API_URL: 'https://pixabay.com/api/?key='
 };
